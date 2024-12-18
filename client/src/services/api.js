d5d463ab02a54c22bd191b40b0ab2e38cb7e7da8
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2024-12-18 07:27:10 +0530

    fix bugs on api routes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://skilltank-server.onrender.com";
 
-export const registerUser = (data) => axios.post(`https://skilltank-server.onrender.com/auth/register`, data);
-export const loginUser = (data) => axios.post(`https://skilltank-server.onrender.com/auth/login`, data);
+export const registerUser = (data) => axios.post(`${API_URL}/register`, data);
+export const loginUser = (data) => axios.post(`${API_URL}/login`, data);
 
